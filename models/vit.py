98356b31c1660f1d066186a3507c7fd36d92a47d
HosseinAghagol <161206318+HosseinAghagol@users.noreply.github.com>
2024-10-01 01:34:30 +0330

    Add files via upload

--- models/vit.py
+++ models/vit.py
@@ -117,7 +117,7 @@ class ViT(nn.Module):
         self.layers.extend([ViTEncoder(opt.E, opt.E*2, opt.H) for _ in range(1,opt.L)])
             
         # Classification head
-        self.classifier_head = nn.Sequential(nn.Linear(opt.E, opt.E),
+        self.classifier_head = nn.Sequential(nn.Linear(opt.E, opt.E*2),
                                              nn.ReLU(),
                                              nn.Linear(opt.E, opt.num_class))
 
