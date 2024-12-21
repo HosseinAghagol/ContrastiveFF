dedc6c238cfad92ea1e4f3c32fe79fe5f906d5d5
HosseinAghagol <161206318+HosseinAghagol@users.noreply.github.com>
2024-12-21 16:48:32 +0330

    Add files via upload

--- losses.py
+++ losses.py
@@ -85,21 +85,21 @@ class FFLoss(nn.Module):
         
         return torch.log(1 + torch.exp(torch.cat([-g_pos + self.threshold, g_neg - self.threshold]))).mean()
     
-class FFLoss(nn.Module):
-    def __init__(self, opt):
-        super(FFLoss, self).__init__()
-        self.threshold = opt.threshold
+# class FFLoss(nn.Module):
+#     def __init__(self, opt):
+#         super(FFLoss, self).__init__()
+#         self.threshold = opt.threshold
 
-    def forward(self, a_pos, a_neg):
-        g_pos = a_pos.pow(2).mean(1)
-        g_neg = a_neg.pow(2).mean(1)
+#     def forward(self, a_pos, a_neg):
+#         g_pos = a_pos.pow(2).mean(1)
+#         g_neg = a_neg.pow(2).mean(1)
         
-        # Numerical stability adjustment
-        logits = torch.cat([-g_pos + self.threshold, g_neg - self.threshold])
-        max_logit = logits.max()
-        logits_stable = logits - max_logit  # Stabilize values to avoid large exp values
+#         # Numerical stability adjustment
+#         logits = torch.cat([-g_pos + self.threshold, g_neg - self.threshold])
+#         max_logit = logits.max()
+#         logits_stable = logits - max_logit  # Stabilize values to avoid large exp values
 
-        return max_logit + torch.log(1 + torch.exp(logits_stable)).mean()
+#         return max_logit + torch.log(1 + torch.exp(logits_stable)).mean()
     
 class SymBaLoss(nn.Module):
 
