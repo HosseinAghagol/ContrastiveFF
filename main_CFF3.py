302b8d56e458b083cc45e4b8d3f622fda99d8973
HosseinAghagol <161206318+HosseinAghagol@users.noreply.github.com>
2025-04-17 03:45:11 +0330

    Add files via upload

--- main_CFF3.py
+++ main_CFF3.py
@@ -35,7 +35,6 @@ def one_epoch_stage1(loader, model, criterions, optimizers, args, phase='train')
         for l in range(args.L):
             x1 = model.layers[l](x1.detach())
             x2 = model.layers[l](x2.detach())
-            print(x1.shape)
             loss = criterions[l]([x1.mean(1),x2.mean(1)], targets)
 
             if phase=='train':
@@ -130,7 +129,7 @@ def main():
                     mbconv_shrinkage_rate = 0.25,     # shrinkage rate of squeeze-excitation in MBConv
                     dropout = 0.1                     # dropout
                 ).to('cuda')
-    model = nn.DataParallel(model)
+    # model = nn.DataParallel(model)
     args.L = len(model.layers)
 
     # build optimizer
